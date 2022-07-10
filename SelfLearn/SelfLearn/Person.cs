f13ca4c0c97913a3182d2b888bcb0b2ed377a6b3
Ishan Madhawa <ishan@ninodigitalhouse.com>
2022-07-10 22:40:48 +0530

    class and encapusulation basic added

---
+++ SelfLearn/SelfLearn/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfLearn
+{
+   class Person
+    {
+        public string name;
+        public int age;
+        private int height;
+        private int weight;
+        private double salary;
+
+        public int Height { get => height; set => height = value; }
+        public int Weight { get => weight; set => weight = value; }
+        public double Salary { get => salary; set => salary = value; }
+
+        public void PrintPersonInfor()
+        {
+            Console.WriteLine("Person Name " + name);
+            Console.WriteLine("Person Age " + age);
+            Console.WriteLine("Person Height " + height);
+            Console.WriteLine("Person Weight " + weight);
+        }
+
+        public void Eating()
+        {
+
+        }
+        public void Sleeping()
+        {
+
+        }
+    }
+}
---
+++ SelfLearn/SelfLearn/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SelfLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Person p1 = new Person();
+           Person p2 = new Person();
+            p1.name = "Ishan";
+            p1.age = 25;
+            p1.Height = 6;
+            p1.Weight = 75;
+            Console.WriteLine(p1.Weight);
+            p1.PrintPersonInfor();
+        }
+    }
+}
